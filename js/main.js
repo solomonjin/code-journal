db92d378215e7dc3186bbc3980666c27e974cd3f
Solomon Jin <solomonjin135@yahoo.com>
2021-05-24 17:46:11 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -25,6 +25,48 @@ function handleNewSubmit(event) {
   var $newPhoto = document.querySelector('.new-entry-img');
   $newPhoto.setAttribute('src', 'images/placeholder-image-square.jpg');
   $newEntryForm.reset();
-
-  return newEntry;
 }
+
+function generateEntryDOM(entry) {
+// <li>
+//   <div class="row">
+//     <div class="column-half img-container">
+//       <img src="">
+//     </div>
+//     <div class="column-half">
+//       <h2></h2>
+//       <p></p>
+//     </div>
+//   </div>
+// </li>
+
+  var $newImage = document.createElement('img');
+  $newImage.setAttribute('src', entry.image);
+
+  var $imageBox = document.createElement('div');
+  $imageBox.className = 'column-half img-container';
+  $imageBox.appendChild($newImage);
+
+  var $newTitle = document.createElement('h2');
+  $newTitle.textContent = entry.title;
+
+  var $newDescription = document.createElement('p');
+  $newDescription.innerText = entry.notes;
+
+  var $textBox = document.createElement('div');
+  $textBox.className = 'column-half';
+  $textBox.appendChild($newTitle);
+  $textBox.appendChild($newDescription);
+
+  var $listContents = document.createElement('div');
+  $listContents.className = 'row';
+  $listContents.appendChild($imageBox);
+  $listContents.appendChild($textBox);
+
+  var $newListEntry = document.createElement('li');
+  $newListEntry.appendChild($listContents);
+
+  return $newListEntry;
+}
+
+generateEntryDOM(data.entries[1]);
